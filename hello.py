e7e5a76304ecd4caae71b64d14fd84e9f36e7344
aarinside <77651459+aarinside@users.noreply.github.com>
2021-01-26 09:38:40 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+server = Flask(name)
+
+
+@server.route("/")
+def hello():
+    return "Hello Sumate Happy ........."
+
+
+if name == "main":
+    server.run(host='0.0.0.0', port=80)
