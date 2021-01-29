8b08363aaccab17cc2d520d4ef598b14da2ebda9
aarinside <6106021611193@fitm.ac.th>
2021-01-29 11:22:18 +0700

    Test github

--- lecture01/hello12345.py
+++ lecture01/hello12345.py
@@ -1,11 +1,11 @@
 from flask import Flask
-server = Flask(name)
+server = Flask(__name__)
 
 
 @server.route("/")
 def hello():
-    return "Hello Sumate Happy ........."
+    return "Hello Kritsadeeka "
 
 
-if name == "main":
+if __name__ == "__main__":
     server.run(host='0.0.0.0', port=80)
